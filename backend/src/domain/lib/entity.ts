830fa8a712304f267490d9b23d88a1a32d6924a6
Emanuel Friedrich <aemanuelfriedrich@gmail.com>
2022-09-30 19:46:35 -0300

    link button to /new-post

--- backend/src/domain/lib/entity.ts
+++ backend/src/domain/lib/entity.ts
@@ -2,11 +2,11 @@ interface Props {
   [index: string]: any;
 }
 
-export default class Entity {
+export default class Entity<T = number> {
 
   public props: Props;
 
-  constructor(public readonly id: number, props: Props) {
+  constructor(public readonly id: T, props: Props) {
     this.props = props;
   }
 
--- frontend/src/components/propertylist.js
+++ frontend/src/components/propertylist.js
@@ -1,10 +1,13 @@
-import { Box, Button, Divider, Paper, Typography } from "@mui/material";
+import { Box, Button, Divider } from "@mui/material";
 import React from "react";
 import Checkbox from "@mui/material/Checkbox";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useNavigate } from "react-router-dom";
 
 const Propertylist = () => {
+  const navigate = useNavigate();
+
   return (
     <div
       style={{
@@ -20,6 +23,7 @@ const Propertylist = () => {
             variant="contained"
             color="error"
             sx={{ borderRadius: "10px" }}
+            onClick={() =>  navigate("/new-post")}
           >
             Nueva Publicación
           </Button>
