f91ac214b4d5f6688c695aedf1b05008d063040e
mariano <cirilliluis@gmail.com>
2022-10-15 20:57:16 -0300

    four step image preview fix

--- frontend/src/components/FourStep.js
+++ frontend/src/components/FourStep.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import Stack from "@mui/material/Stack";
 import ProgressButtons from "./button";
-import { Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 import ButtonUnstyled from "@mui/base/ButtonUnstyled";
 import { Box } from "@mui/system";
 import "./fourstep.css";
 import CloudUploadRoundedIcon from "@mui/icons-material/CloudUploadRounded";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+import ImageUploading from "react-images-uploading";
 
 const FourStep = () => {
-  const [image, setImage] = useState(null);
+  const [image, setImage] = React.useState([]);
+  const [plans, setPlans] = React.useState([]);
 
-  const onImageChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      setImage(URL.createObjectURL(event.target.files[0]));
-    }
+  const onImageChange = (imageList) => {
+    setImage(imageList);
   };
+  const onPlansChange =(plansList) => {
+    setPlans(plansList)
+  }
+
   console.log("paso4");
   return (
     <div>
@@ -32,23 +36,71 @@ const FourStep = () => {
         </Box>
       </Box>
       <Box sx={{ display: "flex" }}>
-        <ButtonUnstyled
-          component="label"
-          className="box-image"
-          sx={{ textDecoration: "none" }}
+        <ImageUploading
+          multiple
+          value={image}
+          onChange={onImageChange}
+          dataURLKey="data_url"
         >
-          <input type="file" multiple onChange={onImageChange} hidden />
-          <CloudUploadRoundedIcon sx={{ fontSize: 40 }} color="error" />
-          <Typography
-            sx={{ fontWeight: "bold", textAlign: "center", fontSize: "14px" }}
-          >
-            Arrastrá o agregá las fotos de la Propiedad
-          </Typography>
-        </ButtonUnstyled>
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            <div className="upload__image-wrapper">
+              <ButtonUnstyled
+                component="label"
+                className="box-image"
+                sx={{ textDecoration: "none" }}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                
+                <CloudUploadRoundedIcon sx={{ fontSize: 40 }} color="error" />
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    fontSize: "14px",
+                  }}
+                >
+                  Arrastrá o agregá las fotos de la Propiedad
+                </Typography>
+              </ButtonUnstyled>
+              {imageList.map((image, index) => (
+              <div key={index} className="box-image_preview" style={{backgroundImage:"url('" + image.data_url + "')"}}>
+                
+                <div className="image-item__btn-wrapper">
+                <div className="image-remove_item">
+                  <button onClick={onImageRemove} className='button-remove'>Quitar</button>
+                </div>
+                </div>
+              </div>
+            ))}
+            </div>
+          )}
+        </ImageUploading>
+
+
+
         <image src={image} alt="preview image" />
-        <Paper elevation={1} sx={{ display: "flex" ,ml:'15%'}} className='advise-style'>
-          <TipsAndUpdatesIcon color="error" fontSize="large" sx={{height:'200px', mr:'20px'}} />
-          <Typography sx={{ color: "#858484" ,fontSize:'14px', alignSelf:'center'}}>
+        <Paper
+          elevation={1}
+          sx={{ display: "flex" }}
+          className="advise-style"
+        >
+          <TipsAndUpdatesIcon
+            color="error"
+            fontSize="large"
+            sx={{ height: "200px", mr: "20px" }}
+          />
+          <Typography
+            sx={{ color: "#858484", fontSize: "14px", alignSelf: "center" }}
+          >
             Para que tu inmueble tenga más visitas, te recomendamos cargar fotos
             nítidas y bien iluminadas. Evitá incluir bordes, logos, marcas de
             agua o textos promocionales.
@@ -56,40 +108,71 @@ const FourStep = () => {
         </Paper>
       </Box>
 
-
       <Box>
-        <Typography sx={{ fontSize: "24px", mt:'100px' }}>
+        <Typography sx={{ fontSize: "24px", mt: "100px" }}>
           <b>Planos(Opcional)</b>
         </Typography>
         <Box>
           <Typography sx={{ color: "#858484", mt: "30px" }}>
-          Cargá los planos de la propiedad. Una vez cargados, arrastrá y soltá para cambiarlos de orden. Se admiten los formatos jpg, jpeg y png desde 500 x 500 px hasta 6000 x 6000 px.
+            Cargá los planos de la propiedad. Una vez cargados, arrastrá y soltá
+            para cambiarlos de orden. Se admiten los formatos jpg, jpeg y png
+            desde 500 x 500 px hasta 6000 x 6000 px.
           </Typography>
         </Box>
       </Box>
       <Box sx={{ display: "flex" }}>
-        <ButtonUnstyled
-          component="label"
-          className="box-image_2"
-          sx={{ textDecoration: "none" }}
+      <ImageUploading
+          multiple
+          value={plans}
+          onChange={onPlansChange}
+          dataURLKey="data_url"
         >
-          <input type="file" multiple onChange={onImageChange} hidden />
-          <CloudUploadRoundedIcon sx={{ fontSize: 40 }} color="error" />
-          <Typography
-            sx={{ fontWeight: "bold", textAlign: "center", fontSize: "14px" }}
-          >
-            Arrastrá o agregá los planos de la Propiedad
-          </Typography>
-        </ButtonUnstyled>
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            <div className="upload__image-wrapper">
+              <ButtonUnstyled
+                component="label"
+                className="box-image_2"
+                sx={{ textDecoration: "none" }}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                
+                <CloudUploadRoundedIcon sx={{ fontSize: 40 }} color="error" />
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    fontSize: "14px",
+                  }}
+                >Arrastrá o agregá los planos de la Propiedad
+                </Typography>
+              </ButtonUnstyled>
+              {imageList.map((image, index) => (
+              <div key={index} className="box-image_preview" style={{backgroundImage:"url('" + image.data_url + "')"}}>
+                
+                <div className="image-remove_item">
+                  <button onClick={onImageRemove} className='button-remove'>Quitar</button>
+                </div>
+              </div>
+            ))}
+            </div>
+          )}
+        </ImageUploading>
+            
+         
         <image src={image} alt="preview image" />
-        
       </Box>
-      
 
-
-
-      <Stack spacing={2} direction="row" sx={{justifyContent:'end'}}>
-        <ProgressButtons/>
+      <Stack spacing={2} direction="row" sx={{ justifyContent: "end" }}>
+        <ProgressButtons />
       </Stack>
     </div>
   );
